6180c008cbff475bf64dbc5fe3b9b54d9e672f16
Kevin Jimenez <keke1422@gmail.com>
2016-03-17 15:00:16 +0100

    Optimization

--- src/main/java/fr/keke142/hshop/listeners/ShopDestroyListener.java
+++ src/main/java/fr/keke142/hshop/listeners/ShopDestroyListener.java
@@ -30,56 +30,70 @@ public class ShopDestroyListener implements Listener {
   public void onBlockBreak(BlockBreakEvent e) {
     Block b = e.getBlock();
     Player p = e.getPlayer();
+
     Shop shop = shopManager.getShopAt(b.getLocation());
     if (shop == null) {
       return;
     }
+
     if (!p.hasPermission("hshop.create")) {
+      p.sendMessage(Lang.PREFIX.toString() + Lang.NOPERMISSION.toString());
       return;
     }
+
     if (!p.getUniqueId().toString().equals(shop.getPlayerUuid())) {
+      p.sendMessage("Pas ton shop");
       e.setCancelled(true);
       return;
     }
 
+    p.sendMessage(Lang.PREFIX.toString()
+        + Lang.SHOPDESTROY.toString().replaceAll("%x", Integer.toString(b.getX()))
+            .replaceAll("%y", Integer.toString(b.getY()))
+            .replaceAll("%z", Integer.toString(b.getZ()))
+            .replaceAll("%world", b.getWorld().getName()));
     shopManager.destroyShop(shop);
-    e.getPlayer().sendMessage(
-        Lang.PREFIX.toString()
-            + Lang.SHOPDESTROY.toString().replaceAll("%x", Integer.toString(b.getX()))
-                .replaceAll("%y", Integer.toString(b.getY()))
-                .replaceAll("%z", Integer.toString(b.getZ()))
-                .replaceAll("%world", b.getWorld().getName()));
-    ItemStack stack = HcGson.deserializeItemStack(shop.getItemSerialized());
-    stack.setAmount(shop.getItemCount());
     if (shop.getItemCount() <= 0) {
       return;
     }
+    ItemStack stack = HcGson.deserializeItemStack(shop.getItemSerialized());
+    stack.setAmount(shop.getItemCount());
     Bukkit.getWorld(shop.getWorldName()).dropItemNaturally(b.getLocation(), stack);
 
   }
 
   @EventHandler
   public void onBlockPhysics(BlockPhysicsEvent e) {
-    Block b = e.getBlock();
-    if (b.getType() != Material.WALL_SIGN || b.getType() != Material.SIGN_POST) {
+    final Block b = e.getBlock();
+    if (b.getType() != Material.WALL_SIGN && b.getType() != Material.SIGN_POST) {
       return;
     }
 
-    Shop shop = shopManager.getShopAt(b.getLocation());
+    final Shop shop = shopManager.getShopAt(b.getLocation());
     if (shop == null) {
       return;
     }
     Sign s = (Sign) b.getState().getData();
-    Block attachedBlock = b.getRelative(s.getAttachedFace());
-    if (attachedBlock.getType() == Material.AIR) {
+    if (b.getRelative(s.getAttachedFace()).getType() == Material.AIR) {
       shopManager.destroyShop(shop);
-      ItemStack stack = HcGson.deserializeItemStack(shop.getItemSerialized());
-      stack.setAmount(shop.getItemCount());
+
+      if (Bukkit.getPlayer(shop.getPlayerName()) != null) {
+        Bukkit.getPlayer(shop.getPlayerName()).sendMessage(
+            Lang.PREFIX.toString()
+                + Lang.SHOPDESTROY.toString().replaceAll("%x", Integer.toString(b.getX()))
+                    .replaceAll("%y", Integer.toString(b.getY()))
+                    .replaceAll("%z", Integer.toString(b.getZ()))
+                    .replaceAll("%world", b.getWorld().getName()));
+      }
       if (shop.getItemCount() <= 0) {
         return;
       }
+      if (shop.isItemSerialized() == false) {
+        return;
+      }
+      ItemStack stack = HcGson.deserializeItemStack(shop.getItemSerialized());
+      stack.setAmount(shop.getItemCount());
       Bukkit.getWorld(shop.getWorldName()).dropItemNaturally(b.getLocation(), stack);
-
     }
   }
 }
